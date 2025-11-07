f52508b0ec6e9b59274e7775ab9540c527657c65
Neetesh Prajapati <139002314+Neetesh26@users.noreply.github.com>
2025-11-08 00:08:33 +0530

    indexSignature

---
+++ index_Signature.ts
@@ -0,0 +1,12 @@
+type userDataType ={
+    name:string,
+    id:number,
+    [key:string] : string|number
+}
+
+var usersData:userDataType={
+    name:"neet",
+    id:6464,
+    address : "vjhvjcdhj",
+    mob:4564456
+}
